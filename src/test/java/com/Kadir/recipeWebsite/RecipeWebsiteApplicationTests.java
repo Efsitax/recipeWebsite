3c429fe2fd57d51320072b35778dad826b179f9c
derslik3-12 <derslik3-12@sinif3-12-iMac.local>
2023-05-10 14:23:01 +0300

    Initial commit

---
+++ src/test/java/com/Kadir/recipeWebsite/RecipeWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kadir.recipeWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
